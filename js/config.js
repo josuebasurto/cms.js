10a69f3ac6060df175102a2931ea080892ec5d42
Josue Basurto <josue@Josh.local>
2016-12-24 10:11:01 -0800

    Change js/config.js for own repo

--- js/config.js
+++ js/config.js
@@ -3,20 +3,20 @@ $(function() {
   CMS.init({
 
     // Name of your site or location of logo file, relative to root directory (img/logo.png)
-    siteName: 'My Site',
+    siteName: 'Josh CMS.js POC',
 
     // Tagline for your site
-    siteTagline: 'Your site tagline',
+    siteTagline: 'Testing simple things... :)',
 
     // Email address
-    siteEmail: 'your_email@example.com',
+    siteEmail: 'josuebasurto@gmail.com',
 
     // Name
-    siteAuthor: 'Your Name',
+    siteAuthor: 'Josh B.',
 
     // Navigation items
     siteNavItems: [
-      { name: 'Github', href: 'https://github.com/yourname', newWindow: false},
+      { name: 'Github', href: 'https://github.com/josuebasurto', newWindow: false},
       { name: 'About'}
     ],
 
@@ -53,8 +53,8 @@ $(function() {
 
      // If Github mode is set, your Github username and repo name.
     githubUserSettings: {
-      username: 'yourusername',
-      repo: 'yourrepo'
+      username: 'josuebasurto',
+      repo: 'cms.js'
     },
 
     // If Github mode is set, choose which Github branch to get files from.
